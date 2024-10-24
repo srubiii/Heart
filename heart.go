8ae47dec144592c74af1e812782336fa147f8e82
Sarah <sarahkrubi@gmail.com>
2024-10-24 18:14:27 -0400

    Heart Final

--- heart.go
+++ heart.go
@@ -7,16 +7,20 @@ import (
 	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
+
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
-type Game struct{}
+type Heart struct {
+	screenWidth  int
+	screenHeight int
+}
 
-func (g *Game) Update() error {
+func (h *Heart) Update() error {
 	return nil
 }
 
-func (g *Game) DrawHalfCircle(screen *ebiten.Image, cx, cy, radius float64, color color.RGBA) {
+func (h *Heart) DrawHalfCircle(screen *ebiten.Image, cx, cy, radius float64, color color.RGBA) {
 	// Draw a half-circle using small lines
 	segments := 30.0        // The number of segments to approximate the curve (converted to float64)
 	startAngle := math.Pi   // Start at 180 degrees (left)
@@ -35,30 +39,31 @@ func (g *Game) DrawHalfCircle(screen *ebiten.Image, cx, cy, radius float64, colo
 	}
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
+func (h *Heart) Draw(screen *ebiten.Image) {
 	// Draw heart shape
 	red := color.RGBA{R: 255, G: 0, B: 0, A: 255}
 
 	// Draw half-circles for the top part of the heart
-	g.DrawHalfCircle(screen, 100, 100, 50, red) // Left half-circle
-	g.DrawHalfCircle(screen, 200, 100, 50, red) // Right half-circle
+	h.DrawHalfCircle(screen, 100, 100, 50, red) // Left half-circle
+	h.DrawHalfCircle(screen, 200, 100, 50, red) // Right half-circle
 
 	// Draw the lines to form the bottom part of the heart
-	vector.StrokeLine(screen, 50, 100, 150, 200, 2, red, false)  // Left line
-	vector.StrokeLine(screen, 250, 100, 150, 200, 2, red, false) // Right line
+	vector.StrokeLine(screen, 50, 100, 150, 210, 2, red, false)  // Left line
+	vector.StrokeLine(screen, 250, 100, 150, 210, 2, red, false) // Right line
+
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return 500, 500
+func (h *Heart) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 300, 300
 }
 
 func main() {
 	log.Printf("main: Hello World")
 	fmt.Println("Hello There")
 
-	ebiten.SetWindowSize(500, 500)
-	ebiten.SetWindowTitle("Heart Shape with Half Circles")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	ebiten.SetWindowSize(300, 300)
+	ebiten.SetWindowTitle("Heart Shape")
+	if err := ebiten.RunGame(&Heart{}); err != nil {
 		log.Fatal(err)
 	}
 }
